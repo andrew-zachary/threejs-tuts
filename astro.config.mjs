85181abf147200338e58e3766b91dedc99c01f54
andrew-zachary <andrew_zachary@outlook.com>
2023-10-03 00:00:00 +0000

    create the home-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    server: {
-      port: 4200
-    }
+  server: {
+    port: 4200
   },
   integrations: [tailwind()]
-});
+})
